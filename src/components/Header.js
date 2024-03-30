3978c53a290e43dda71fab16da755e39ec92bd60
giwoong <dev.choi0409@gmail.com>
2024-03-30 23:46:08 +0900

    refactor: header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import logoImage from "../assets/Logo.png";
 import { LogoAndTextContainer, Logo, Text } from "../css/HeaderCss";
 
 export default function Header() {
   return (
     <header>
-      <LogoAndTextContainer>
-        <Logo alt="Logo" src={logoImage} />
-        <Text>
-          CODE
-          <br />
-          CHAINING
-        </Text>
-      </LogoAndTextContainer>
+      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+        <LogoAndTextContainer>
+          <Logo alt="Logo" src={logoImage} />
+          <Text>
+            CODE
+            <br />
+            CHAINING
+          </Text>
+        </LogoAndTextContainer>
+      </Link>
     </header>
   );
 }
