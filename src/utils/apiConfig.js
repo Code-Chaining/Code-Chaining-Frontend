0599f97c9c214d7eb649005ec5b66e05f3271fb8
giwoong <dev.choi0409@gmail.com>
2024-04-05 00:04:35 +0900

    feat: axios 인터셉터를 사용한 토큰 자동 갱신, 요청 재시도 로직 구현

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -18,3 +18,34 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 403 && localStorage.getItem("refreshToken")) {
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const response = await axios.post(`${apiBaseUrl}/token/access`, {
+          refreshToken: refreshToken,
+        });
+
+        const accessToken = response.data.data.accessToken;
+        localStorage.setItem("accessToken", accessToken);
+        error.config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+        return axiosInstance(error.config);
+      } catch (refreshError) {
+        alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+
+        localStorage.clear();
+        window.location = "/login";
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
