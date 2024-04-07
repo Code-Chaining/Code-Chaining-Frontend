26c67f54191751c985a769dc2f896b2712161db4
giwoong <dev.choi0409@gmail.com>
2024-04-07 17:21:11 +0900

    refactor: 댓글 css, 댓글 없을 시 메시지 추가

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -5,9 +5,12 @@ import {
   CommentContent,
   CommentItem,
   CommentsFormContainer,
+  WriterAndDate,
   Writer,
+  Date,
   CommentInput,
   CommentContents,
+  NoCommentsMessage,
 } from "../css/CommentsContainerCss";
 
 export default function CommentsContainer({
@@ -25,68 +28,84 @@ export default function CommentsContainer({
 }) {
   return (
     <CommentsFormContainer>
-      {comments.map((comment) => (
-        <CommentItem key={comment.commentId}>
-          <ProfileImage src={comment.picture} alt="Profile" />
-          <CommentContent>
-            <Writer>{comment.nickname}</Writer>
-            {isCommentEditing && editingCommentId === comment.commentId ? (
-              <div>
-                <CommentInput
-                  type="text"
-                  placeholder="댓글을 입력하세요."
-                  value={editedContents}
-                  onChange={(e) => onUpdateContents(e)}
-                />
-              </div>
-            ) : (
-              <CommentContents>{comment.contents}</CommentContents>
-            )}
-
-            {isLoggedIn && userInfo.memberId === comment.memberId ? (
-              <>
-                {!isCommentEditing || editingCommentId !== comment.commentId ? (
-                  <ButtonContainer>
-                    <Button
-                      $variant="edit"
-                      type="button"
-                      onClick={() => onEdit(comment)}
-                    >
-                      수정
-                    </Button>
-                    <Button
-                      $variant="delete"
-                      type="button"
-                      onClick={() => onDelete(comment.commentId)}
-                    >
-                      삭제
-                    </Button>
-                  </ButtonContainer>
+      {comments.length > 0 ? (
+        <>
+          {comments.map((comment) => (
+            <CommentItem key={comment.commentId}>
+              <ProfileImage src={comment.picture} alt="Profile" />
+              <CommentContent>
+                <WriterAndDate>
+                  <Writer>{comment.nickname}</Writer>
+                  <Date>{comment.date}</Date>
+                </WriterAndDate>
+                {isCommentEditing && editingCommentId === comment.commentId ? (
+                  <div>
+                    <CommentInput
+                      type="text"
+                      placeholder="댓글을 입력하세요."
+                      value={editedContents}
+                      onChange={(e) => onUpdateContents(e)}
+                    />
+                  </div>
                 ) : (
-                  <ButtonContainer>
-                    <Button
-                      $variant="cancel"
-                      type="button"
-                      onClick={() => onCancel()}
-                    >
-                      취소
-                    </Button>
-                    <Button
-                      $variant="save"
-                      type="submit"
-                      onClick={() => onUpdate(editingCommentId, editedContents)}
-                    >
-                      저장
-                    </Button>
-                  </ButtonContainer>
+                  <CommentContents>{comment.contents}</CommentContents>
                 )}
-              </>
-            ) : (
-              <></>
-            )}
-          </CommentContent>
-        </CommentItem>
-      ))}
+
+                {isLoggedIn && userInfo.memberId === comment.memberId ? (
+                  <>
+                    {!isCommentEditing ||
+                    editingCommentId !== comment.commentId ? (
+                      <ButtonContainer>
+                        <Button
+                          $variant="edit"
+                          type="button"
+                          onClick={() => onEdit(comment)}
+                        >
+                          수정
+                        </Button>
+                        <Button
+                          $variant="delete"
+                          type="button"
+                          onClick={() => onDelete(comment.commentId)}
+                        >
+                          삭제
+                        </Button>
+                      </ButtonContainer>
+                    ) : (
+                      <ButtonContainer>
+                        <Button
+                          $variant="cancel"
+                          type="button"
+                          onClick={() => onCancel()}
+                        >
+                          취소
+                        </Button>
+                        <Button
+                          $variant="save"
+                          type="submit"
+                          onClick={() =>
+                            onUpdate(editingCommentId, editedContents)
+                          }
+                        >
+                          저장
+                        </Button>
+                      </ButtonContainer>
+                    )}
+                  </>
+                ) : (
+                  <></>
+                )}
+              </CommentContent>
+            </CommentItem>
+          ))}
+        </>
+      ) : (
+        <NoCommentsMessage>
+          댓글이 없습니다!
+          <br />
+          댓글을 작성하여 함께 토론해보세요!
+        </NoCommentsMessage>
+      )}
     </CommentsFormContainer>
   );
 }
