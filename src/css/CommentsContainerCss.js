eb2d37ea755a5f583daf38a8bd42be15a0eed11d
giwoong <dev.choi0409@gmail.com>
2024-04-10 01:30:30 +0900

    feat: 공개 방 검색 기능 구현

--- src/css/CommentsContainerCss.js
+++ src/css/CommentsContainerCss.js
@@ -69,7 +69,7 @@ export const CommentInput = styled.textarea`
   background-color: #ffffff;
   border-radius: 0.4vw;
   font-size: 1.1vw;
-  resize: vertical;
+  resize: none;
 
   &:focus {
     outline: none;
